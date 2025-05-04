17599ecfb38c85a0b025c37dc727b9d681455e54
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-05-04 23:59:00 +0500

    add base logic of kazans and tuzdyks

---
+++ src/main/java/org/example/board/Kazans.java
@@ -0,0 +1,8 @@
+package org.example.board;
+
+public class Kazans {
+    public static final int WHITE_KAZAN = 0;
+    public static final int BLACK_KAZAN = 1;
+
+    private final int[] kazans;
+}
---
+++ src/main/java/org/example/board/Tuzdyks.java
@@ -0,0 +1,8 @@
+package org.example.board;
+
+public class Tuzdyks {
+    public static final int WHITE_TUZDYK = 0;
+    public static final int BLACK_TUZDYK = 1;
+
+    private final int[] tuzdyks;
+}
