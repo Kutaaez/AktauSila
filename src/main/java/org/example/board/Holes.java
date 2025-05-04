ab9ac7f19576783aa9a42e184db748141407b054
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-05-04 23:52:00 +0500

    added methods for hole

--- src/main/java/org/example/board/Holes.java
+++ src/main/java/org/example/board/Holes.java
@@ -5,4 +5,20 @@ public class Holes {
     public static final int HOLES_PER_PLAYER = 9;
     public static final int INITIAL_SEEDS = 9;
     public static final int TUZDYK_MARKER = 255;
+    private final int[] holes;
+
+    public Holes() {
+        holes = new int[TOTAL_HOLES];
+        reset();
+    }
+
+    public int getSeedCount(int index) {
+        validateIndex(index);
+        return holes[index];
+    }
+
+    public void setSeedCount(int index, int count) {
+        validateIndex(index);
+        holes[index] = count;
+    }
 }
