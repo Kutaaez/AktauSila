6408425e351ee7b5427882a2b9f650ec01e2c98c
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-05-04 23:50:00 +0500

    added holes, currentplayer

---
+++ src/main/java/org/example/board/Holes.java
@@ -0,0 +1,8 @@
+package org.example.board;
+
+public class Holes {
+    public static final int TOTAL_HOLES = 18;
+    public static final int HOLES_PER_PLAYER = 9;
+    public static final int INITIAL_SEEDS = 9;
+    public static final int TUZDYK_MARKER = 255;
+}
