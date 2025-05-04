337bcbb2894ad0c372c23fcf881ebcd070d7de5f
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-05-04 23:59:00 +0500

    + methods for  kazans and tuzdyks

--- src/main/java/org/example/board/Kazans.java
+++ src/main/java/org/example/board/Kazans.java
@@ -4,5 +4,22 @@ public class Kazans {
     public static final int WHITE_KAZAN = 0;
     public static final int BLACK_KAZAN = 1;
 
-    private final int[] kazans;
+    public Kazans() {
+        kazans = new int[2];
+    }
+
+    public int getKazan(int playerColor) {
+        validatePlayerColor(playerColor);
+        return kazans[playerColor];
+    }
+
+    public void addToKazan(int playerColor, int seeds) {
+        validatePlayerColor(playerColor);
+        kazans[playerColor] += seeds;
+    }
+
+    public void reset() {
+        kazans[WHITE_KAZAN] = 0;
+        kazans[BLACK_KAZAN] = 0;
+    }
 }
--- src/main/java/org/example/board/Tuzdyks.java
+++ src/main/java/org/example/board/Tuzdyks.java
@@ -5,4 +5,23 @@ public class Tuzdyks {
     public static final int BLACK_TUZDYK = 1;
 
     private final int[] tuzdyks;
+    public Tuzdyks() {
+        tuzdyks = new int[2];
+        reset();
+    }
+
+    public int getTuzdyk(int playerColor) {
+        validatePlayerColor(playerColor);
+        return tuzdyks[playerColor];
+    }
+
+    public void setTuzdyk(int playerColor, int holeIndex) {
+        validatePlayerColor(playerColor);
+        tuzdyks[playerColor] = holeIndex;
+    }
+
+    public void reset() {
+        tuzdyks[WHITE_TUZDYK] = 0;
+        tuzdyks[BLACK_TUZDYK] = 0;
+    }
 }
