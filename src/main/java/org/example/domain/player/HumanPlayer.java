290204531df18eedd96324c6e8e7b24cbdb674c1
Aibolll <amantaiaibol13@gmail.com>
2025-05-06 00:00:00 +0500

    finalize player logic implemneted facade pattern for classes

--- src/main/java/org/example/domain/player/HumanPlayer.java
+++ src/main/java/org/example/domain/player/HumanPlayer.java
@@ -1,5 +1,7 @@
 package org.example.domain.player;
 
+import org.example.domain.facade.ToguzBoard;
+
 public class HumanPlayer implements IPlayer {
     private final int color;
 
@@ -7,7 +9,10 @@ public class HumanPlayer implements IPlayer {
         this.color = color;
     }
 
-
+    @Override
+    public int makeMove(ToguzBoard board) {
+        return -1;
+    }
 
     @Override
     public int getColor() {
