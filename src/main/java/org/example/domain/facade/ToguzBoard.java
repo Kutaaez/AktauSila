197c32546780b7b19d05679d0bc06549c4dbcdd9
Aibolll <amantaiaibol13@gmail.com>
2025-05-05 19:50:00 +0500

    add facade design pattern create cass Toguzboard

---
+++ src/main/java/org/example/domain/facade/ToguzBoard.java
@@ -0,0 +1,17 @@
+package org.example.domain.facade;
+
+import org.example.domain.board.BoardState;
+import org.example.domain.board.CurrentPlayer;
+import org.example.domain.board.Holes;
+import org.example.domain.strategy.MoveStrategyFactory;
+import org.example.domain.observer.IStateObserver;
+import org.example.domain.game.IGameState;
+import org.example.domain.game.GameStateImpl;
+import org.example.domain.strategy.IMoveStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToguzBoard {
+
+}
